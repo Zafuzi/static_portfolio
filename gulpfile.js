bccac7a1354cce35ea4e3cc16b1883ddbbd57396
Zafuzi <zacharyfoutz@gmail.com>
2017-02-11 15:12:12 -0800

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    less = require('gulp-less'),
+    minifyCSS = require('gulp-csso'),
+    watch = require('gulp-watch');
+
+gulp.task('html', function() {
+    return watch('client/templates/*.pug', function() {
+        gulp.src('client/templates/*.pug')
+            .pipe(pug())
+            .pipe(gulp.dest('build/html'))
+    });
+});
+
+gulp.task('css', function() {
+    return watch('client/templates/*.less', function() {
+        gulp.src('client/templates/*.less')
+            .pipe(less())
+            .pipe(minifyCSS())
+            .pipe(gulp.dest('build/css'))
+    });
+});
+
+gulp.task('default', ['html', 'css']);
